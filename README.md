ec37f22ad700ac62c2b4d7d3cf2360ddfa4e3846
me <me@devuan>
2020-08-06 20:05:21 -0400

    Display Standard and Extendend ASCII charts.

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-`hexdump.exe` - Display a file in hex+ASCII format.
+`hexdump.exe` - Display a file in hex+ASCII format; or display Standard ASCII chart or Extended ASCII chart.
 
 `hexdump.zip` is a FreeDOS package containing `hexdump.exe` (and sources).
 
 To install: `fdinst install hexdump.zip`. To install with sources: `fdnpkg install-wsrc hexdump.zip`
 
+The Standard ASCII chart (characters 0-127) and the Extended ASCII chart (characters 128-255) will each fit on an 80x24 screen. A binary table of values 0-0xF is also printed with each ASCII chart.
+
 `hexdump.exe` was created to run on (16-bit) FreeDOS, but can also be built to run on (64-bit) Linux. When built on Linux, it is named `hexd` in order to avoid conflicting with the existing `hexdump` command.
+
+On Linux, the characters displayed on the Extended ASCII chart will depend on the character encoding of the terminal. Setting the encoding to [CodePage 437](https://en.wikipedia.org/wiki/Code_page_437) should produce what would be seen in FreeDOS. CodePage 850 produces similar, but not exact, results.
 
 `hexdump.exe` (and `hexd`) gives exactly the same output as the Linux command `hexdump -C`
 
@@ -19,5 +23,5 @@ On FreeDOS:
 On Linux:
 
     ./build.sh clean     Remove generated files
-    ./build.sh hexdump   Build the hexd executable
+    ./build.sh hexd      Build the hexd executable
     ./build.sh zip       Store files for FreeDOS in h.zip
--- hexdump.c
+++ hexdump.c
@@ -1,26 +1,31 @@
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #if !defined(uint)
     typedef unsigned int uint;
 #endif
 
+
+#define MAX_X   80
+#define MAX_Y   24
+#define ITEMS  128
+
+#define TEXT_LEN 5
+
+#define STD_CHART 0
+#define EXT_CHART 1
+
 int
-main ( int argc, char** argv )
+dumpfile( char * fname )
 {
     char ch;
     uint i;
     uint k;
     FILE * fp;
-    char * fname;
     char asc[17]; // 16 + 1 for null.
 
-    if (argc < 2) {
-        printf("Display file in hex+ASCII format. A file must be provided.\n");
-        exit(1);
-    }
-    
-    fname = argv[1];
     asc[16] = '\0';
     if (( fp = fopen(fname, "rb") )) {
         i = 0;
@@ -63,4 +68,139 @@ main ( int argc, char** argv )
         return 1;
     }
     return 0;
+}
+
+
+// Modifies buf to hold a string representation of val.
+// Assumes buf can hold, at least, 4 chars.
+void
+val_to_str( int val, char * buf )
+{
+    char names[][4] = {
+        "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
+        "BS",  "TAB", "LF",  "VT",  "FF",  "CR",  "SO",  "SI",
+        "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
+        "CAN", "EM",  "SUB", "ESC", "FS",  "GS",  "RS",  "US",
+        "spc"
+    };
+    if (val <= ' ') {
+        sprintf(buf, "%s", names[val]);
+    } else if (val == 127) {
+        sprintf(buf, "DEL");
+    } else {
+        sprintf(buf, "%c", val);
+    }
+}
+
+
+int
+get_text_width( int chart, int col )
+{
+    int width;
+    int pad = 4;
+
+    if (chart == EXT_CHART) {
+        return 1 + pad;
+    }
+
+    if (col <= 1) {
+        width = 3 + pad;
+    } else if (col == 5) {
+        width = 3;
+    } else {
+        width = 1 + pad;
+    }
+    return width;
+}
+
+
+int
+get_val_width( int chart, int col )
+{
+    if (chart == EXT_CHART) {
+        return 3;
+    }
+    return (col >= 4 ? 3 : 2);
+}
+
+void
+print_binary_str(int val)
+{
+    int i;
+    printf("    %X  ", val);
+    for (i = 8; i >= 1; i >>= 1) {
+        printf("%c", (val & i ? '1' : '0'));
+    }
+}
+
+
+int
+print_ascii_grid( int chart )
+{
+    int cols = ITEMS / MAX_Y;
+    int col;
+    int row;
+    int val;
+    int val_width;
+    char text[TEXT_LEN];
+    int text_width;
+    int limit = (chart == STD_CHART ? 128 : 256);
+
+    if (ITEMS % MAX_Y > 0) {
+        cols += 1;
+    }
+    for (row = 0; row < MAX_Y; row++) {
+        for (col=0; col<cols; col++) {
+            val = row + (col * MAX_Y) + (limit - 128);
+            val_width = get_val_width(chart, col);
+            text_width = get_text_width(chart, col);
+            if (val < limit) {
+                val_to_str(val, text);
+                printf("%*d x%02X %-*s", val_width, val, val, text_width, text);
+            } else {
+                print_binary_str(val-limit);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+void
+help( char * progname )
+{
+    printf("Display file in hex+ASCII format. Or display an ASCII chart.\n");
+    printf("Usage: %s [/E] [/H] [/?]\n", progname);
+    printf("       %s FILENAME\n", progname);
+    printf("\n");
+    printf("            If no option is supplied, display Standard ASCII chart (0-127)\n");
+    printf("  /E        Display Extended ASCII chart (128-255)\n");
+    printf("  /H, /?    Display this help\n");
+    printf("  FILENAME  File to display in hex+ASCII format\n");
+    printf("\n");
+    printf("A binary table of values 0-0xF is also printed with each ASCII chart.\n");
+    printf("The characters displayed in the Extended ASCII chart will depend on the\n");
+    printf("codepage that is currently loaded. The DOS command 'CHCP' will display the\n");
+    printf("current codepage.\n");
+}
+
+
+int
+main ( int argc, char** argv )
+{
+    if (argc == 1) {
+        return print_ascii_grid(0);
+    }
+
+    if (strcmp(argv[1], "/h") == 0 || strcmp(argv[1], "/H") == 0 || strcmp(argv[1], "/?") == 0) {
+        help(basename(argv[0]));
+        return 0;
+    } else if (strcmp(argv[1], "/a") == 0 || strcmp(argv[1], "/A") == 0) {
+        return print_ascii_grid(STD_CHART);
+    } else if (strcmp(argv[1], "/e") == 0 || strcmp(argv[1], "/E") == 0) {
+        return print_ascii_grid(EXT_CHART);
+    } else {
+        return dumpfile(argv[1]);
+    }
 }
